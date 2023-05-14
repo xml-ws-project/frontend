1c5eb8e44901e574a65ce2365f155aa92e7ae778
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2023-05-14 19:36:53 +0200

    resolved conflicts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { NavigationStart, Router } from '@angular/router'
 import { Subscription } from 'rxjs'
+import { AuthService } from './auth/services/auth.service'
 
 export let browserRefresh = false
 
@@ -11,11 +12,17 @@ export let browserRefresh = false
 })
 export class AppComponent implements OnInit {
   title = 'VIMA Booking'
+  isLogged = false
   private refreshSub: Subscription
+  private userSub: Subscription | undefined
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.autoLogin()
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isLogged = !!user
+    })
     this.refreshSub = this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
         browserRefresh = !this.router.navigated
--- src/app/main-app/main-app-routing.module.ts
+++ src/app/main-app/main-app-routing.module.ts
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
 import { Routes, RouterModule } from '@angular/router'
 import { NgModule } from '@angular/core'
 import { NotfoundPageComponent } from '../shared/errors/components/notfound-page/notfound-page.component'
 import { RequestsPageComponent } from '../reservation-requests/components/requests-page/requests-page.component'
 import { AddAccomComponent } from '../add-accom/add-accom.component'
+import { LandingPageComponent } from '../accommodation/components/landing-page/landing-page.component'
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: LandingPageComponent,
-  //   title: 'VIMA Airlines',
-  // },
+  {
+    path: '',
+    component: LandingPageComponent,
+    title: 'VIMA Airlines',
+  },
   {
     path: '*',
     component: NotfoundPageComponent,
@@ -25,32 +25,6 @@ const routes: Routes = [
     title: 'VIMA Booking | Add Accommodation',
   },
   // {
-=======
-import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { LandingPageComponent } from '../accommodation/components/landing-page/landing-page.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: LandingPageComponent,
-    title: 'VIMA Booking',
-  },
-  // {
-  //   path: '*',
-  //   component: NotfoundPageComponent,
-  // },
-  // {
-  //   path: 'search-result',
-  //   component: MainSearchResComponent,
-  // },
-  // {
-  //   path: 'create-flight',
-  //   component: CreateFlightPageComponent,
-  //   title: 'VIMA Airlines | Create Flight',
-  // },
-  // {
->>>>>>> 8c243738a481e2e49d300894e7e623357ff4f11e
   //   path: 'show-flight/:id',
   //   component: ShowFlightPageComponent,
   //   title: 'VIMA Airlines | Flight',
@@ -67,19 +41,11 @@ const routes: Routes = [
   //   path: 'search-result',
   //   component: MainSearchResComponent,
   // },
-<<<<<<< HEAD
 ]
-=======
-];
->>>>>>> 8c243738a481e2e49d300894e7e623357ff4f11e
 
 @NgModule({
   declarations: [],
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-<<<<<<< HEAD
 export class MainAppRoutingModule {}
-=======
-export class MainAppRoutingModule { }
->>>>>>> 8c243738a481e2e49d300894e7e623357ff4f11e
