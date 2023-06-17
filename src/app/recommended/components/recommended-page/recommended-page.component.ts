2e7fed33d6ca569d2ccf7cd6fae0be9b227df95b
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2023-06-17 02:45:23 +0200

    added route and recommendation page

---
+++ src/app/recommended/components/recommended-page/recommended-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended-page',
+  templateUrl: './recommended-page.component.html',
+  styleUrls: ['./recommended-page.component.scss']
+})
+export class RecommendedPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
